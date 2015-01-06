428fbf2dd7fff7cfb14b6a8d38cad0c23866f2f6
jarettsims <j@jarettsims.com>
2015-01-06 10:49:45 -0500

    configured connection file to work locally and on DO

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,12 +1,13 @@
 require 'active_record'
 
+if ENV["DB_INFO"] 
 ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/novelteam')
-
-# ActiveRecord::Base.establish_connection({
-#   :adapter => "postgresql", #can work with any type of sql database. List the type here.
-#   :host => "localhost", #running it on the ip listed address (in this place localhost, whcih is a synonym for 127.0.0.1)
-#   :username => "Jarett", # your psql username
-#   :database => "novelteam" #we're explicitly giving it the name of the database we want to connect to.
-# })
-
 ActiveRecord::Base.logger = Logger.new(STDOUT)
+else
+ActiveRecord::Base.establish_connection({
+  :adapter => "postgresql", #can work with any type of sql database. List the type here.
+  :host => "localhost", #running it on the ip listed address (in this place localhost, whcih is a synonym for 127.0.0.1)
+  :username => "Jarett", # your psql username
+  :database => "novelteam" #we're explicitly giving it the name of the database we want to connect to.
+})
+end
